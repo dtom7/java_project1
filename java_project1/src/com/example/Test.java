ea7ac8a98ce496e41b0ee3e5800c9587c229d86b
Dony Thomas <dony.thomas7@gmail.com>
2014-12-12 22:00:12 -0500

    third commit

--- java_project1/src/com/example/Test.java
+++ java_project1/src/com/example/Test.java
@@ -1,13 +1,13 @@
 package com.example;
 
+import java.text.DateFormat;
 import java.util.Date;
 
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Current date/time is: " + new Date());
-		
-
+		DateFormat df = DateFormat.getInstance();
+		System.out.println("Current date/time is: " + df.format(new Date()));
 	}
 
 }
