d0f8abf6eff4359df4fafefd2be8a8eacb69af3d
Dony Thomas <dony.thomas7@gmail.com>
2014-12-12 21:48:35 -0500

    second commit

--- java_project1/src/com/example/Test.java
+++ java_project1/src/com/example/Test.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world !!!");
+		System.out.println("Current date/time is: " + new Date());
+		
 
 	}
 
