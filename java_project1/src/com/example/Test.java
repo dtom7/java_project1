79946481eaa203bea24554b75af925d7e111aa54
Dony Thomas <dony.thomas7@gmail.com>
2014-12-12 22:17:53 -0500

    fourth commit

--- java_project1/src/com/example/Test.java
+++ java_project1/src/com/example/Test.java
@@ -2,12 +2,14 @@ package com.example;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Test {
 
 	public static void main(String[] args) {
 		DateFormat df = DateFormat.getInstance();
 		System.out.println("Current date/time is: " + df.format(new Date()));
+		System.out.println("Default locale is " + Locale.getDefault());
 	}
 
 }
